1636efd626b5e65cb8e1e38440d1a848327009dd
mmstarz <frace@ukr.net>
2019-11-26 16:58:32 +0200

    fixing

--- routes/mainRoutes.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require("express");
-const mainController = require("../controllers/mainController");
-const router = Router();
-
-router.get("/", mainController.getHomePage);
-
-module.exports = router;
